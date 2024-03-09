92e90aae94e908f886a665bdc34720aedf599215
Oleksandr Marchenko <sasha@mate.academy>
2024-03-10 00:42:18 +0200

    make build

---
+++ index.f0f43a9f.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#sidebar"===window.location.hash?document.body.classList.add("page__content--with-sidebar"):document.body.classList.remove("page__content--with-sidebar")});
+//# sourceMappingURL=index.f0f43a9f.js.map
